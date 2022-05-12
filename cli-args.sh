fd31771947e62b84bba67c324a091e475c06c77f
orion3 <noreply@orion3>
2022-05-13 02:55:44 +0700

    WIP: parse() function, added pseudo associative & regular arrays support to make it easier to use dynamic names for them

--- cli-args.sh
+++ cli-args.sh
@@ -1,8 +1,197 @@
 #!/usr/bin/env bash
 source $BASHJAZZ_PATH/utils/array.sh
+source $BASHJAZZ_PATH/utils/formatting.sh
 source $BASHJAZZ_PATH/utils/quoted_string.sh
 
+declare -g CLIARGS_RSEP='' # U+001E, record separator
+declare -g CLIARGS_USEP='' # U+001F, unit separator
+
+NULL_SYM='␀'
+
 CliArgs() {
+
+  extract_synonyms() {
+    echo $(echo "${@}" | \
+      sed -E 's/:[^ ]+/ /g')
+  }
+
+  get_name() {
+    # This is "just in case" someone passes us an argument name which has:
+    #
+    # 1) One or two leading dash "-" charachters in the front - then we simply
+    #    strip them.
+    #
+    # 2) An underscore "_" separating words instead of the "dash". We replace
+    # it with a dash "-" because all argument names in CliArgs() are stored
+    # with underscores replacing dashes in their names, such that, for example,
+    # the name "input-fn" becomes "input_fn".
+    local arg_name=$(echo "$1" | sed 's/^--//' | sed 's/-/_/g')
+
+    local arg_names_var="ARG_DEF_all_args_FOR_$CLIARGS_NS"
+    local arg_synonyms_var="ARG_DEF_synonyms_FOR_$CLIARGS_NS"
+    local names=(
+      $(echo "${!arg_names_var}" | sed -E 's/[a-zA-Z0-9_]+[:]//g')
+    )
+    local synonyms=(  $(echo "${!arg_synonyms_var}") )
+
+    local result="$(echo "${names[@]}"   | \
+      grep -oE "(^|\s|:)$arg_name(\s|$)" | \
+      sed 's/^://')"
+
+    if [[ -z "$result" ]]; then
+      local item_index="$(Array_get_index_for $arg_name ${synonyms[@]})"
+      result="${names[$item_index]}"
+    fi
+
+    echo "$result" | xargs # again, xargs here is to remove trailing whitespace
+
+  }
+
+  get_value() {
+    # TODO
+    return 0
+  }
+
+  declare_namespace_var() {
+    if [[ "$1" == "-"* ]]; then
+      local var_flag="$1 "
+      shift
+    fi
+    local var_name="$1_FOR_$CLIARGS_NS"
+    shift
+    declare -g "$var_name"="${var_flag}$(echo "$@" | xargs)"
+  }
+
+  append_to_namespace_var() {
+    var_name="$1_FOR_$CLIARGS_NS"
+    shift
+
+    local var_addition="$(echo "$@" | xargs)"
+    local var_type="$(echo "${!var_addition}" | grep -oE '^-[a-zA-Z]')"
+
+    local var_value
+    if [[ $var_type == '-A' ]]; then
+      var_value="${!var_name}$var_addition${CLIARGS_RSEP}"
+    elif [[ $var_type == '-a' ]]; then
+      var_value="${!var_name} $var_addition"
+    else
+      # The difference from the above is that when we append to a string
+      # we don't separate with a space.
+      var_value="${!var_name}$var_addition"
+    fi
+    printf -v "$var_name" '%s' "$var_value"
+  }
+
+  # $1 = arr_name (without the namespace, example: ARG_DEF_all_args)
+  # $2 = arr_index (integer)
+  get_namespace_arr_item() {
+    var_name="${2}_FOR_${CLIARGS_NS}"
+
+    if [[ "${!var_name}" == "-A"* ]]; then
+      local i="$1"
+      echo "${!var_name}" | \
+        grep -oE "${i}${CLIARGS_USEP}[^$CLIARGS_RSEP]+" | \
+        sed "s/${i}${CLIARGS_USEP}//"
+    else
+      local i=$(($1+1)) # Because 0 is -a
+      local arr=( $(echo "${!var_name}" ) )
+      echo "${arr[$i]}"
+    fi
+
+  }
+
+  extract_arg_name() {
+    echo "$1" | grep -o '^\-\-[a-zA-Z0-0\-]+=' | \
+      sed 's/^--//' | sed 's/-/_/g' | sed 's/=$//'
+  }
+
+  extract_arg_value() {
+    echo "$1" | \
+      sed -E "s/--[a-z\-]+=?//g" | \
+      sed 's/,/ /g'              | \
+      sed 's/-/_/g'
+  }
+
+  define() {
+
+    local allowed=(
+      no_value_args default_value_args value_args required_args required_values \
+      ignore_errors
+    )
+    local args=( ${@} )
+    local out
+    local all_args
+    local no_value_args
+    local value_args
+    local required_args
+
+    for arg in "${args[@]}"; do
+
+      local arg_name="$(extract_arg_name "$arg")"
+      Array_contains $arg_name "${allowed[@]}" > /dev/null || continue
+
+      local value="$(extract_arg_value "$arg")"
+
+      if [[ "$arg_name" =~ ^((no_)?value|required)_(args|values)$ ]]; then
+        if [[ "$arg_name" != "required_args" ]]; then
+          all_args+="$value "
+        fi
+        value="$(echo "$value" | sed -E 's/(^| )[a-zA-Z0-9]+:/ /g')"
+        printf -v "$arg_name" '%s' "${!arg_name} $value"
+      else
+        printf -v "$arg_name" '%s' "$value"
+      fi
+    done
+
+    local synonyms="$(extract_synonyms "$all_args")"
+    declare_namespace_var 'ARG_DEF_all_args'        "$all_args"
+    declare_namespace_var 'ARG_DEF_no_value_args'   "$no_value_args"
+    declare_namespace_var 'ARG_DEF_value_args'      "$value_args"
+    declare_namespace_var 'ARG_DEF_required_args'   "$required_args"
+    declare_namespace_var 'ARG_DEF_required_values' "$required_values"
+    declare_namespace_var 'ARG_DEF_synonyms'        "$synonyms"
+    declare_namespace_var 'ARG_DEF_ignore_errors'   "$ignore_errors"
+  }
+
+  parse() {
+
+    while $# -gt 0; do
+      arg="$1"
+      shift
+
+      # Determine argument type: a 1-dash argument, a 2-dash argument or a
+      # positional argument.
+      local arg_type
+      if [[ "$arg" == "--"* ]]; then
+        arg_type=2
+      elif [[ "$arg" == "-"* ]]; then
+        arg_type=1
+      else
+        [[ -n $prev_arg_name ]] && arg_type=-1 || arg_type=0
+      fi
+
+      if [ $arg_type -gt 0 ]; then
+        local arg_name="$(extract_arg_name "$arg")"
+        local arg_value="$(extract_arg_value "$arg")"
+      else
+        if [ $arg_type = -1 ]; then
+          append_to_namespace_var 'arg_values' "$prev_arg_name" "$arg_name"
+        else
+          append_to_namespace_var 'pos_arg_values' "$arg"
+        fi
+      fi
+
+    done
+
+  }
+
+  print_error() {
+    local arg_name="$1"
+    local arg_value="$2"
+    local title="$3"
+    echo -e "$(ind 4; color red)ERROR: $title"
+    echo -e "$(ind 8; color bold) $arg_name == $arg_value$(color off)"
+  }
 
   # The First ARGUMENT to this function is the name of the nested function to
   # be called. The actual calls will be done after all function declarations.
@@ -18,141 +207,25 @@ CliArgs() {
   # User may want to parse arguments for various scripts or even separate
   # functions inside each script which may all be a part of a larger program.
   # Therefore, we need to store arguments data for each call separately.
-  # This will be done via associative arrays where each key is the NAME of the
+  # This will be done via associative arrays - each key is the CLIARGS_NS of the
   # caller and each value is a pseudo-array string. Users of CliArgs() won't
   # have to worry about these details, this is all internal.
   #
-  # NAME can be provided by the user himself via the CALLER variable in front
-  # of the actual invocation of `CliArgs()` or it'll be assigned automatically,
+  # _NS stands for NAMESPACE. The variable CLIARGS_NS can be provided by
+  # user via the CALLER variable in front of the actual invocation of `CliArgs()`
+  # or it'll be assigned automatically,
   # by fetching caller-script filename. If, for some, reason automatic assignment
-  # fails, NAME will given the value "default".
+  # fails, CLIARGS_NS will given the value of "default".
   #
-  local NAME="$CALLER" # can only contain alphanumeric characters and "_"
-  if [[ -z "$NAME" ]]; then
+  # CLIARGS_NS can only contain alphanumeric chars and _
+
+  if [[ -z "$CALLER" ]]; then
     caller | grep -ovE '^[0-9]+' | grep -ovE '.[a-zA-Z]$' | sed 's/-/_/g' |
-      read _ NAME # reads caller script name into the $NAME variable
-    NAME="${NAME:-default}"
+      read _ CLIARGS_NS # reads caller script name into the $CLIARGS_NS variable
   fi
+  CLIARGS_NS="${CALLER:-default}"
 
-  extract_synonyms() {
-    echo $(echo "${@}" | \
-      sed -E 's/:[^ ]+/ /g')
-  }
-
-  get_arg_name() {
-    local arg_name=$(echo $1 | sed 's/-/_/g')
-    local arg_names=( ${!ARG_DEF_all_args_FOR_$NAME} )
-    local synonyms=(  ${!ARG_DEF_synonyms_FOR_$NAME} )
-
-    local real_arg_name="$(echo "${arg_names[@]}" | grep -o "$arg_name")"
-
-    if [[ -z "$real_arg_name" ]]; then
-      local item_index=$(${arg_synonyms[@]/$arg_name//} | \
-        cut -d/ -f1 | wc -w | tr -d ' ')
-    fi
-
-    local real_arg_name="${arg_names[$item_index]}"
-    echo "$real_arg_name"
-  }
-
-  get_value_for() {
-    # TODO
-    return 0
-  }
-
-  add_arg_data_for_name() {
-    local var_prefix="$1"
-    # xargs removes leading/trailing whitespace here
-    local global_var_value="$(echo $2 | xargs)"
-    local global_var_name="ARG_DEF_${var_prefix}_FOR_$NAME"
-
-    declare -g "$global_var_name"="$global_var_value"
-    echo "$global_var_name=\"${global_var_value:-$ARG_VALUE_TRUE}\""
-  }
-
-  define() {
-
-    local allowed=(
-      no_value_args default_value_args value_args required_args ignore_errors
-    )
-    local args=( ${@} )
-    local out
-    local all_args
-
-    for k in "${!args[@]}"; do
-
-      local arg="${args[$k]}"
-
-      local arg_name="$(echo "$arg" | \
-        grep -o '^\-\-[a-z\-]*' | sed 's/^--//' | sed 's/-/_/g')"
-
-      local value="$(echo "$arg" | sed -E "s/--[a-z\-]+=?//g" | sed 's/,/ /g')"
-
-      Array_contains $arg_name "${allowed[@]}" > /dev/null || continue
-
-      if [[ "$arg_name" =~ ^(no_)?value_args$ ]]; then
-        all_args+="$value "
-      fi
-
-      # The next line of code removes the ":" character, which means we only
-      # keeps the two-dash argument names in these variables or,
-      # if a cli-arg only has a one-dash version, then that's the one that will
-      # be kept. This is fine, because get_value_for() will take either a
-      # synonym or an actual name and return a value we'll extract later on.
-      value="$(echo "$value" | sed -E 's/(^| )[a-zA-Z]:/ /g')"
-      echo "$(add_arg_data_for_name $arg_name "$value")"
-    done
-
-    local synonyms="$(extract_synonyms "$all_args")"
-    echo "$(add_arg_data_for_name 'all_args' "$all_args")"
-    echo "$(add_arg_data_for_name 'synonyms' "$synonyms")"
-  }
-
-  parse() {
-
-    local name="${1:-$NAME}"
-
-    while $# -gt 0; do
-      arg="$1"
-      shift
-
-      # $arg is either a one-dash or a two-dash argument such as
-      # -i or --input-fn
-      if [[ "$arg" == "-"* ]]; then
-        echo 0 > /dev/null
-      # Case for when it's something like my-script -i file.txt
-      # and $arg == "file.txt"
-      elif [[ -n "$value_for_prev_arg" ]]; then
-        echo 0 > /dev/null
-      # This is a positional argument
-      else
-        echo 0 > /dev/null
-      fi
-
-      # Process $arg in several steps:
-      # 1. If argument starts with a dash or two dashes - it's non-positional:
-      #
-      #      1) Determine if it's allowed
-      #      2) Determine if it requires value and assign it.
-      #      3) Determine if value is optional, assign default or provided.
-      #      4) Determine if value is not allowed
-      #         (which means an argument may be followed by a positional argument).
-      #
-      # 2. If arguments starts with an alphanumeric character:
-      #
-      #    1) Check if previous argument was a one-dash argument that was
-      #       expecting a value. If yes, then this argument IS NOT a positional
-      #       argument, but a value to the non-positional one-dash argument.
-      # 
-      #    2) Otherwise treat this argument as a positional argument.
-
-    done
-
-  }
-
-  # ATTENTION: here the program will exit if a non-existent nested function
-  # was called.
   $CALL_NESTED $@
-  exit $?
+  return $?
 
 }
--- cli-args.test.sh
+++ cli-args.test.sh
@@ -1,40 +1,74 @@
 #!/usr/bin/env bash
 
-source $BASHJAZZ_PATH/utest/utest.sh
-source $BASHJAZZ_PATH/utils/assign_vars_from_out.sh
 source cli-args.sh
+source $BASHJAZZ_PATH/utils/assign_vars_from_out.sh
+source $BASHJAZZ_PATH/utest/utest.sh
+
+# This "sets the stage" for all unit test, as the will be checking against
+# the same set of arguments and synonyms below.
+CliArgs define \
+  --no-value-args='a,l:list,u:list-updates,version,b' \
+  --value-args='x,i:input-fn,o:output-fn,log-fn,y' \
+  --required-args='a' \
+  --required-values='input-fn' \
+  --args='x,i:input-fn,o:output-fn,log-fn,y' > /dev/null
 
 utest begin CliArgs \
 'Parses and separates cli-args into positional & non-positional'
 
-  utest begin define \
-  'Defines the rules according to which arguments are parsed and accepted/rejected'
-    utest cmd CliArgs define \
-      --no-value-args='a,l:list,u:list-updates,version,b' \
-      --value-args='x,i:input-fn,o:output-fn,log-fn,y' \
-      --ignore-errors
-    echo "$UTOUT" | assign_vars_from_out
-    utest assert "$ARG_DEF_no_value_args_FOR_default" == 'a list list-updates version b'
-    utest assert "$ARG_DEF_value_args_FOR_default"    == 'x input-fn output-fn log-fn y'
-    utest assert "$ARG_DEF_ignore_errors_FOR_default" == 'yes'
-    utest assert "$ARG_DEF_synonyms_FOR_default"      == \
-      'a l u version b x i o log-fn y'
-  utest end define
+  utest begin declare_namespace_var
+    assoc="greeting${CLIARGS_USEP}hello${CLIARGS_RSEP}"
+    assoc+="address${CLIARGS_USEP}world${CLIARGS_RSEP}"
+    CliArgs declare_namespace_var str_var 'hello world'
+    CliArgs declare_namespace_var -a arr_var 'hello world'
+    CliArgs declare_namespace_var -A assoc_var "$assoc"
+    unset assoc
+
+    utest assert "$str_var_FOR_default"  == 'hello world'
+    utest assert "$(utest get_namespace_arr_item 0 arr_var)" == 'hello'
+    utest assert "$(utest get_namespace_arr_item 1 arr_var)" == 'world'
+    utest assert "$(utest get_namespace_arr_item greeting assoc_var)" == 'hello'
+    utest assert "$(utest get_namespace_arr_item address  assoc_var)" == 'world'
+  utest end declare_namespace_var
+
+  utest begin append_to_declared_var_for_name
+    utest cmd CliArgs declare_var_for_name str_var 'hello world'
+    utest cmd CliArgs declare_var_for_name arr_var 'hello world'
+    utest cmd CliArgs declare_namespace_var -A assoc_var \
+      "greeting${CLI_ARGS_USEP} hello${CLIARGS_RSEP} address${CLI_ARGS_USEP} world"
+    utest cmd CliArgs append_to_namespace_var
+  utest end append_to_declared_var_for_name pending
+
+  #utest begin define \
+  #'Defines the rules according to which arguments are parsed and accepted/rejected'
+    #utest assert "$ARG_DEF_ignore_errors_FOR_default" == 'yes'
+    #utest assert "$ARG_DEF_all_args_FOR_default" == \
+      #'a l:list u:list_updates version b x i:input_fn o:output_fn log_fn y'
+    #utest assert "$ARG_DEF_no_value_args_FOR_default" == \
+      #'a list list_updates version b'
+    #utest assert "$ARG_DEF_value_args_FOR_default" == \
+      #'x input_fn output_fn log_fn y'
+    #utest assert "$ARG_DEF_required_args_FOR_default" == 'a'
+    #utest assert "$ARG_DEF_synonyms_FOR_default" == \
+      #'a l u version b x i o log_fn y'
+  #utest end define pending
 
   #utest begin parse
 
     #utest begin with_correct_args
-      #utest add_cmd CliArgs define \
-        #--no-value-args='a,l:list,u:list-updates,version,b' \
-        #--value-args='x,i:input-fn,o:output-fn,log-fn,y' \
-        #--required-args='input-fn' \
-      #utest add_cmd CliArgs parse -a -l --list-updates --version \
-        #-i file.txt 'positional argument value'
-      #utest cmd
-      #echo "$UTOUT" | assign_vars_from_out
+      #utest cmd CliArgs parse -a -l --list-updates --version \
+        #-i file.txt --output-fn=output.txt
+      #utest assert "${ARGS_VALUES_FOR_default[a]}"            == 'yes'
+      #utest assert "${ARGS_VALUES_FOR_default[list]}"         == 'yes'
+      #utest assert "${ARGS_VALUES_FOR_default[list_updates]}" == 'yes'
+      #utest assert "${ARGS_VALUES_FOR_default[input.txt]}"    == 'input.txt'
+      #utest assert "${ARGS_VALUES_FOR_default[output.txt]}"   == 'output.txt'
     #utest end with_wrong_args
 
-    #utest begin with_wrong_args
+    #utest begin with_uknown_args
+    #utest end with_uknown_args pending
+
+    #utest begin with_wrong_arg_values
     #utest end with_wrong_arg_values pending
 
     #utest begin without_required_arg
@@ -42,28 +76,64 @@ utest begin CliArgs \
 
   #utest end parse
 
-  #utest begin ignoring_unknown_args \
-  #"doesn't throw error when --ignore-unknown-args flag is present"
-    #utest add_cmd CliArgs define \
-      #--ignore-arg-errors
-      #--no-value-args=a,b,c,hello
-    #utest add_cmd CliArgs parse -x --howdy
-    #utest assert "$UTERR" is blank
-  #utest end ignoring_uknown_args
+  utest begin get_name \
+  'Given the synonym or original name in $1 to get_name(), echo original arg name'
 
-  #utest begin get_value_for_TWO_DASH_ARG \
-  #'Gets value from --two-dash-arg, sets to "true" if no value provided after ='
-    #utest cmd CliArgs get_value_for \
-      #two-dash-arg '--two-dash-arg="value 1" --arg2=1'
-    #utest assert "$UTOUT" == 'value 1'
-  #utest end get_value_for_TWO_DASH_ARG
-  #exit
+    utest begin from_the_same_name \
+    'Prints the original arg name, the same value as $1 given to get_name()'
 
-  #utest begin get_value_for_ONE_DASH_ARG \
-  #'Gets value from '
-    #utest cmd CliArgs get_value_for t '-t "value 1" -s something_else'
-    #utest assert "$UTOUT" == 'value 1'
-  #utest end get_value_for_ONE_DASH_ARG
+      # Case 1: list is itself the actual long name of the argument,
+      # CliArgs's `get_name()` must simply return the same value
+      # as it's been passed.
+      utest cmd CliArgs get_name 'list'
+      utest assert "$UTOUT" == 'list'
+
+      # Case 2: argument 'y' has no long-version, thus it isn't a synonym.
+      # even though it may appear to be so.
+      utest cmd CliArgs get_name 'y'
+      utest assert "$UTOUT" == 'y'
+
+      # Case 3: arg name contains a dash "-" char. CliArgs() stores values for
+      # arguments in a Bash associative array, therefore its keys - which are
+      # argument names - mustn't contain dashes; get_name(), therefore,
+      # replaces dashes with underscores "_" upon returning the argument name.
+      utest cmd CliArgs get_name 'input-fn'
+      utest assert "$UTOUT" == 'input_fn'
+    utest end from_the_same_name
+
+    utest begin from_synonym \
+    'Prints the original arg name given synonym provided as $1 to get_name()'
+
+      # Case1: argument 'l' has a long-version 'list', thus it is a synonym.
+      utest cmd CliArgs get_name 'l'
+      utest assert "$UTOUT" == "list"
+
+    utest end from_synonym
+
+  utest end get_name pending
+
+  ######################### PENDING ##############################
+  utest begin get_value
+    utest begin two_dash_argument \
+    'Gets value from --two-dash-arg, sets to "true" if no value provided after ='
+      utest cmd CliArgs get_value_for \
+        two-dash-arg '--two-dash-arg="value 1" --arg2=1'
+      utest assert "$UTOUT" == 'value 1'
+    utest end two_dash_argument pending
+
+
+    utest begin single_dash_argument \
+      'Gets value (or sets it to "true") from a single-dash argument, such as -a'
+      utest cmd CliArgs get_value_for t '-t "value 1" -s something_else'
+      utest assert "$UTOUT" == 'value 1'
+    utest end single_dash_argument pending
+  utest end get_value
+
+  utest begin ignoring_unknown_args \
+  "doesn't throw error when --ignore-unknown-args flag is present"
+    utest add_cmd CliArgs parse -x --input-fn=filename.txt
+    utest assert "$UTERR" is blank
+  utest end ignoring_uknown_args pending
 
 
 utest end CliArgs
